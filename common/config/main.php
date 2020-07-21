aa2c64dd885a70217b338c183a575b1c3f4bd27f
Julian Ramirez <hipogea@hotmail.com>
2020-07-21 16:14:04 -0500

    Instalando el RBAC

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,25 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+        'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\PhpManager'
+           ],
+        
+        'user' => [
+            'class' => 'mdm\admin\models\User',
+        'identityClass' => 'mdm\admin\models\User',
+        'loginUrl' => ['admin/user/login'],
+             ]
+        
+        
     ],
+    
+    'modules' => [
+    'admin' => [
+        'class' => 'mdm\admin\Module',
+        
+    ]
+        ],
+    
+    
 ];
